2a06a26a9f835f0fe278f733da71cfba5a0fb888
Juliandychandra <Juliandychandra2@gmail.com>
2023-03-29 17:37:50 +0700

    Lanjutan 29/03/23

--- app/src/main/java/com/si61/payobejalan/MainActivity.java
+++ app/src/main/java/com/si61/payobejalan/MainActivity.java
@@ -1,18 +1,28 @@
 package com.si61.payobejalan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private FloatingActionButton fabTambah;
     private RecyclerView rvDestinasi;
 
+    private ArrayList<String> arrNama, arrAlamat, arrJam;
+
+    private AdapterDestinasi adDestinasi;
+
+    MyDatabaseHelper myDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,8 +34,42 @@ public class MainActivity extends AppCompatActivity {
         fabTambah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,TambahActivity.class));
+                startActivity(new Intent(MainActivity.this, TambahActivity.class));
             }
         });
+        myDB = new MyDatabaseHelper(MainActivity.this);
+    }
+    private void SQLiteToArrayList(){
+        Cursor varCursor = myDB.bacaDataDestinasi();
+        if(varCursor.getCount() == 0){
+            Toast.makeText(this, "Data Tidak Tersedia", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            while (varCursor.moveToNext()){
+                arrNama.add(varCursor.getString(1));
+                arrAlamat.add(varCursor.getString(2));
+                arrJam.add(varCursor.getString(3));
+
+
+            }
+        }
+    }
+    private void tampilanDestinasi(){
+        arrNama = new ArrayList<>();
+        arrAlamat = new ArrayList<>();
+        arrJam = new ArrayList<>();
+
+        SQLiteToArrayList();
+
+        adDestinasi = new AdapterDestinasi(this, arrNama, arrAlamat, arrJam);
+        rvDestinasi.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        rvDestinasi.setAdapter((adDestinasi));
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        tampilanDestinasi();
     }
 }
